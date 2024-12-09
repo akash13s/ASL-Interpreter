076e8ad85141e6b9d7b9d090cbd18d13cf7cb685
Manisha Goyal <manisha.goyal1996@gmail.com>
2024-12-09 16:46:57 -0500

    updated sbatch params

--- llava-next-video/train-1-gpu.sh
+++ llava-next-video/train-1-gpu.sh
@@ -2,11 +2,11 @@
 #SBATCH --nodes=1                   # 1 node
 #SBATCH --ntasks-per-node=1          # 1 task per node
 #SBATCH --cpus-per-task=1            # 1 CPU per task for 1 GPU
-#SBATCH --time=12:00:00
+#SBATCH --time=6:00:00
 #SBATCH --mem=64GB
-#SBATCH --gres=gpu:1                 # Request 1 GPU per node
-#SBATCH --job-name=video-llava-1-gpu
-#SBATCH --output=video-llava-1-gpu.out
+#SBATCH --gres=gpu:v100:1                 # Request 1 GPU per node
+#SBATCH --job-name=llava_next_video-1-gpu
+#SBATCH --output=llava_next_video-1-gpu.out
 #SBATCH --nice=0
 
 module purge
